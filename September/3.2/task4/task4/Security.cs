99eb259978da36de4a010cbfb5745a6c48bebbcc
aseka <mamedov.agasaf@mail.ru>
2023-09-28 15:25:52 +0400

    commit

---
+++ September/3.2/task4/task4/Security.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    internal class Security : Worker
+    {
+        public string salary { get; set; }
+        public override void print()
+        {
+            Console.WriteLine(salary);
+        }
+    }
+}
